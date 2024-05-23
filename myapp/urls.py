95813351640c32b94f5602e9cb93fb93cc230a30
Mike Cheruiyot <65896352+kimutai-debug@users.noreply.github.com>
2024-05-23 13:34:05 +0300

    clr

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-
-app_name = "myapp"
-
 urlpatterns = [
-    path('', views.index, name='index')
-    ]
+    path('', views.index, name='index'),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
+import os
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+
+
